27d9b1078255ff44504e8266f5be817c08b5d7be
mahdi hajian <mahdi.haj.1375@outlook.com>
2019-04-01 23:44:43 +0430

    add web api

---
+++ WebAppTagHelper/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppTagHelper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public ActionResult<string> Get()
+        {
+            return Ok("mahdi hajian");
+        }
+    }
+}
